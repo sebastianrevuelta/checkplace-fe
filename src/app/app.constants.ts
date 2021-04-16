2fa3a59f661ee3da731a5b76ab14307af70ad91c
sebastianrevuelta <sebastianrevuelta@gmail.com>
2021-04-16 11:35:28 +0200

    add constants

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,4 +1,5 @@
-//export const API_URL = 'http://localhost:30008'
-export const API_URL = 'http://192.168.99.100:30008'
+export const BACKEND_HOST = '192.168.99.100'
+export const BACKEND_PORT = '30008'
+export const API_URL = `http://${BACKEND_HOST}:${BACKEND_PORT}`
 export const JPA_API_URL = `${API_URL}/jpa`
 //export const API_URL = 'https://checkplace.azurewebsites.net'

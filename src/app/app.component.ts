2bbdf60b9d04eb81ab8a007975febc1b83979032
Sebastian Revuelta Marchetti <sebasrevuelta@MacBook-Pro.home>
2023-01-17 10:48:59 +0100

    add comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'checkplace';
-  message = 'an original chess game developed with Angular - Spring boot - Maria DB - Cassandra - Docker images - Kubernetes'
+  message = 'an original chess game developed with Angular - Spring boot - Maria DB - Cassandra - Docker images - Kubernetes - nginx'
 }
